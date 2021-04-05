3bd37b00b4c5a6e08e19b9d2e30b9243f1c2d095
Timur Murtukov <murtukov@gmail.com>
2021-04-05 02:01:02 +0200

    First commit

---
+++ src/main/java/CssConverter.java
@@ -0,0 +1,220 @@
+import com.google.common.primitives.Chars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * TODO:
+ *  1. Simply stringify rule values
+ *  2. Ignore comment blocks
+ *  3. Process font names with spaces correctly
+ */
+public class CssConverter {
+    public StringBuilder buffer = new StringBuilder();
+    public StringBuilder result = new StringBuilder();
+    char[] stoppers             = new char[] {'{', ':', ';', '}'};
+
+    public String parse(String css) {
+        var scope = 0;
+
+        css = normalizeWhitespaces(css);
+
+        for (final char c : css.toCharArray()) {
+            if ('(' == c) {
+                scope++;
+            } else if (')' == c) {
+                scope--;
+            }
+
+            if (Chars.contains(stoppers, c) && scope == 0) {
+                result.append(convertToken(buffer.toString(), c));
+                buffer.setLength(0);
+            } else {
+                buffer.append(c);
+            }
+        }
+
+        return result.toString().trim();
+    }
+
+    private String convertToken(String token, char stopper) {
+        token = token.trim();
+
+        switch (stopper) {
+            case '{': // selector
+                return convertSelector(token);
+            case ':': // rule name
+                return convertRuleName(token);
+            case ';': // rule value
+                return convertRuleValue(token);
+            case '}': // end of block
+                return "}";
+        }
+
+        return "";
+    }
+
+    private String convertSelector(String token) {
+        return "{\n" + token;
+    }
+
+    private String convertRuleName(String token) {
+        if (token.charAt(0) == '-') {
+            return "'"+token+"':";
+        }
+
+        return toCamelCase(token) + ":";
+    }
+
+    private String convertRuleValue(String token) {
+        token = token.trim();
+
+//        if (true) {
+//            return " " + new CssValue(token) + ",\n";
+//        }
+
+        return " " + new CssValue(token) + ",\n";
+    }
+
+    /**
+     * Converts a kebab-cased string into a camelCased one.
+     */
+    private String toCamelCase(String input) {
+        var temp = Arrays.stream(input.split("-"))
+                .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase())
+                .collect(Collectors.joining());
+
+        return Character.toLowerCase(temp.charAt(0)) + temp.substring(1);
+    }
+
+    private String normalizeWhitespaces(String input) {
+        return input.trim().replaceAll("\\s+", " ");
+    }
+
+    /**
+     * Wraps string into single quotes if it's not numeric.
+     */
+    private String maybeWrapIntoQuotes(String input) {
+        if (isNumeric(input)) {
+            return input;
+        }
+
+        return "'" + input + "'";
+    }
+
+    private String removeSuffix(String input) {
+        if (input.endsWith("px")) {
+            return input.substring(0, input.length() - 2);
+        }
+
+        return input;
+    }
+
+    private boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    private String trimQuotes(String input) {
+        return input
+                .replaceAll("^'|'$", "")
+                .replaceAll("^\"|\"$", "");
+    }
+
+    private ArrayList<String> split(String input, char separator) {
+        var buffer = new StringBuilder();
+        var parts  = new ArrayList<String>();
+        var parenthesis  = 0;
+        var singleQuote = false;
+        var doubleQuote = false;
+
+        for (final char c : input.toCharArray()) {
+            if ('(' == c) {
+                parenthesis++;
+            } else if (')' == c) {
+                parenthesis--;
+            }
+
+            if ('\'' == c) {
+                singleQuote = !singleQuote;
+            }
+
+            if ('"' == c) {
+                doubleQuote = !doubleQuote;
+            }
+
+            if (separator == c && 0 == parenthesis && !singleQuote && !doubleQuote) {
+                parts.add(buffer.toString().trim().replace('\n', ' '));
+                buffer.setLength(0);
+            } else {
+                buffer.append(c);
+            }
+        }
+
+        if (buffer.length() > 0) {
+            parts.add(buffer.toString().trim().replace('\n', ' '));
+            buffer.setLength(0);
+        }
+
+        return parts;
+    }
+
+    private class CssValue {
+        private final ArrayList<String> parts;
+
+        CssValue(String input) {
+            this.parts = split(input, ',');
+        }
+
+        @Override
+        public String toString() {
+            var result = new ArrayList<String>();
+
+            for (String part : parts) {
+                part = normalizeWhitespaces(part);
+                var partResult = new ArrayList<String>();
+
+                for (String value : split(part, ' ')) {
+                    partResult.add(
+                        maybeWrapIntoQuotes(
+                            trimQuotes(
+                                removeSuffix(value)
+                            ).replace("'", "\\'")
+                        )
+                    );
+                }
+
+                if (partResult.size() > 1) {
+                    result.add("["+String.join(", ", partResult)+"]");
+                } else {
+                    result.add(String.join(", ", partResult));
+                }
+            }
+
+            if (result.size() > 1) {
+                if (containsArrays(result)) {
+                    return "[\n" + String.join(",\n", result) + "\n]";
+                }
+
+                return "[" + String.join(", ", result) + "]";
+            }
+
+            return String.join(", ", result);
+        }
+
+        private boolean containsArrays(ArrayList<String> input) {
+            for (final var item : input) {
+                if (item.charAt(0) == '[') {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- src/main/java/MyAction.java
+++ src/main/java/MyAction.java
@@ -1,11 +1,25 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
 public class MyAction extends AnAction {
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        // Using the event, create and show a dialog
+        var currentProject = event.getProject();
+        var dlgMsg = new StringBuilder(event.getPresentation().getText() + " Selected!");
+        var dlgTitle = event.getPresentation().getDescription();
 
+        // If an element is selected in the editor, add info about it.
+        var nav = event.getData(CommonDataKeys.NAVIGATABLE);
+
+        if (nav != null) {
+            dlgMsg.append(String.format("\nSelected Element: %s", nav.toString()));
+        }
+
+        Messages.showMessageDialog(currentProject, dlgMsg.toString(), dlgTitle, Messages.getInformationIcon());
     }
 
     @Override
