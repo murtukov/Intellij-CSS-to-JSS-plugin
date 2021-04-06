06019c4e6de5b679c93ae0b7617f11895ca325b4
Timur Murtukov <murtukov@gmail.com>
2021-04-06 14:53:36 +0200

    Fix formatting of pasted text

--- src/main/java/com/murtukov/css_to_jss/PasteAsJssAction.java
+++ src/main/java/com/murtukov/css_to_jss/PasteAsJssAction.java
@@ -4,10 +4,19 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ide.CopyPasteManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.codeStyle.CodeStyleManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
 public class PasteAsJssAction extends AnAction {
     @Override
@@ -16,26 +25,30 @@ public class PasteAsJssAction extends AnAction {
         var contents = cpManager.getContents();
         var project = event.getProject();
         var editor = event.getData(CommonDataKeys.EDITOR);
+        var psiFile = event.getData(CommonDataKeys.PSI_FILE);
         var document = editor.getDocument();
         var primaryCaret = editor.getCaretModel().getPrimaryCaret();
+        var csManager = CodeStyleManager.getInstance(project);
 
-        assert contents != null;
+        var psiDocManager = PsiDocumentManager.getInstance(project);
 
-        var parser = new CssConverter();
+        // My custom converter
+        CssConverter converter = new CssConverter();
 
         WriteCommandAction.runWriteCommandAction(project, () -> {
+            var start = primaryCaret.getSelectionStart();
+            var end = primaryCaret.getSelectionEnd();
+
             try {
-                document.replaceString(
-                    primaryCaret.getSelectionStart(),
-                    primaryCaret.getSelectionEnd(),
-                    parser.parse(contents.getTransferData(DataFlavor.stringFlavor).toString())
-                );
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+                final var string = converter.parse(contents.getTransferData(DataFlavor.stringFlavor).toString());
+                document.replaceString(start, end, string);
+                psiDocManager.commitDocument(document);
+                csManager.reformatText(psiFile, start, start + string.length());
+            } catch (Exception ignored) {}
         });
 
-        primaryCaret.removeSelection();
+
+//        primaryCaret.removeSelection();
     }
 
     @Override
