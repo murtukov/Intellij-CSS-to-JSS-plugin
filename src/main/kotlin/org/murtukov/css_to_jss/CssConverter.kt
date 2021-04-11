ac28de3f2ce92f76b76e6d24bab04703490d4285
Timur Murtukov <murtukov@gmail.com>
2021-04-11 17:02:16 +0200

    Interim

--- src/main/kotlin/org/murtukov/css_to_jss/CssConverter.kt
+++ src/main/kotlin/org/murtukov/css_to_jss/CssConverter.kt
@@ -1,96 +1,80 @@
 package org.murtukov.css_to_jss
 
-import org.murtukov.css_to_jss.util.normalizeWhitespaces
-import org.murtukov.css_to_jss.util.removeCommentBlocks
-import java.lang.NumberFormatException
+import org.murtukov.css_to_jss.util.*
 import java.util.*
+import kotlin.collections.HashMap
+import kotlin.properties.Delegates
 
 class CssConverter {
     private val rules   = arrayOf("margin", "padding")
-    private var current = ""
+    private var lines   = HashMap<CssProperty, CssValue>()
+
+    private lateinit var lastProperty: CssProperty
 
     fun convert(css: String): String {
         val normalizedCss    = css.normalizeWhitespaces().removeCommentBlocks()
-        var parenthesisScope = 0
         val stoppers         = "{:;}"
+        var expectedStoppers = "{:"
+        var parenthesisScope = 0
         var buffer           = ""
-        var result           = ""
 
-        for (c in normalizedCss) {
+        parse@for (c in normalizedCss) {
             when (c) {
                 '(' -> parenthesisScope++
                 ')' -> parenthesisScope--
             }
 
             if (stoppers.contains(c) && 0 == parenthesisScope) {
-                result += when (c) {
-                    '{' -> "{\n"
-                    ':' -> "    " + convertRuleName(buffer.trim()) + ":"
-                    ';' -> " ${CssValue(buffer.trim())},\n"
-                    '}' -> "}"
-                    else -> ""
+                if (!expectedStoppers.contains(c)) {
+                    return css // return unchanged
                 }
+
+                when (c) {
+                    '{' -> expectedStoppers = ":"
+                    ':' -> {
+                        lastProperty = CssProperty(buffer)
+                        expectedStoppers = ";";
+                    }
+                    ';' -> {
+                        lines[lastProperty] = CssValue(buffer, lastProperty)
+                        expectedStoppers = ":}"
+                    }
+                    '}' -> break@parse
+                }
+
                 buffer = ""
             } else {
                 buffer += c
             }
         }
 
-        return result;
+        // Check, if selection was partial
+
+
+        return generate()
     }
 
-    private fun convertRuleName(token: String): String {
-        return when {
-            token.startsWith('-') -> "'$token'"
-            else -> toCamelCase(token).also { current = it }
-        }
-    }
+    private fun generate(): String {
+        val stringifiedRules = lines
+            .map { "${it.key}: ${it.value}," }
+            .joinToString("\n")
+            .prependIndent()
 
-    /**
-     * Converts a kebab-cased string into a camelCased one.
-     */
-    private fun toCamelCase(input: String): String {
-        return input
-            .split('-')
-            .joinToString("") { it.first().toUpperCase() + it.drop(1) }
-            .decapitalize()
+        return "{\n$stringifiedRules\n}"
     }
 
     private fun maybeWrapIntoQuotes(input: String): String {
-        return if (isNumeric(input)) input else "'$input'"
+        return if (input.isNumeric()) input else "'$input'";
     }
 
-    private fun removeSuffix(input: String): String {
-        return if (input.endsWith("px")) {
-            input.dropLast(2)
-        } else {
-            input
-        }
-    }
-
-    private fun isNumeric(str: String): Boolean {
-        return try {
-            str.toDouble()
-            true
-        } catch (e: NumberFormatException) {
-            false
-        }
-    }
-
-    private fun trimQuotes(input: String): String {
-        return input
-            .removeSurrounding("'")
-            .removeSurrounding("\"")
-    }
-
-    private fun split(input: String, separator: Char): ArrayList<String> {
-        var buffer = "";
+    private fun String.split(separator: Char): ArrayList<String> {
+        var buffer = ""
         val parts = ArrayList<String>()
         var parenthesis = 0
         var singleQuote = false
         var doubleQuote = false
 
-        for (c in input) {
+        for (c in this) {
             when (c) {
                 '('  -> parenthesis++
                 ')'  -> parenthesis--
@@ -100,33 +84,65 @@ class CssConverter {
 
             if (separator == c && 0 == parenthesis && !singleQuote && !doubleQuote) {
                 parts.add(buffer.trim().replace('\n', ' '))
-                buffer = "";
+                buffer = ""
             } else {
-                buffer += c;
+                buffer += c
             }
         }
 
         if (buffer.isNotBlank()) {
             parts.add(buffer.trim().replace('\n', ' '))
-            buffer = "";
         }
 
         return parts
     }
 
-    private inner class CssValue(input: String) {
-        private val parts: ArrayList<String> = split(input, ',')
+    private inner class CssProperty(name: String) {
+        val cssName = name
+        val jssName = when {
+            cssName.startsWith('-') -> "'$cssName'"
+            else -> cssName.toCamelCase()
+        }
+
+        override fun toString() = jssName
+    }
+
+    private enum class ValueType {
+        SCALAR,                 // 200px
+        SPACE_SEPARATED,        // 0 0 20px 30px
+        COMMA_SEPARATED,        // "Helvetica Neue", Arial, sams-serif
+        COMMA_SPACE_SEPARATED,  // rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px
+        MIXED                   // italic bold .8em/1.2 Helvetica Neue, Arial, sans-serif
+    }
+
+    private inner class CssValue(rawValue: String, private val property: CssProperty) {
+        private val parts = rawValue.split(',')
+        private var isImportant = false
+        private var defaultUnit = "px"
+        private val valueType: ValueType = ValueType.SCALAR
+
+        init {
+            if (rawValue.contains("!important")) {
+                rawValue.removeSuffix("!important")
+                isImportant = true
+            }
+        }
+
+        /**
+         * 1. check if !important is set
+         * 2. Determine the value type
+         */
 
         override fun toString(): String {
             val result = ArrayList<String>()
 
             for (part in parts) {
-                val part = part.normalizeWhitespaces();
+                val normalized = part.normalizeWhitespaces()
                 val partResult = ArrayList<String>()
-                val values = split(part, ' ')
+                val values     = normalized.split(' ')
 
-                if ("fontFamily" == current) {
-                    partResult.add(processValue(part))
+                if ("fontFamily" == property.jssName) {
+                    partResult.add(processValue(normalized))
                 } else {
                     for (value in values) {
                         partResult.add(processValue(value))
@@ -134,16 +150,14 @@ class CssConverter {
                 }
 
                 if (partResult.size > 1) {
-                    val lastItem = values[partResult.size - 1]
-
-                    if (lastItem == "!important" && rules.contains(current)) {
+                    if (values.last() == "!important" && rules.contains(property.jssName)) {
                         partResult.removeAt(partResult.size - 1)
-                        result.add("[${join(partResult, true)}, '!important']")
+                        result.add("[${partResult.join(true)}, '!important']")
                     } else {
-                        result.add(join(partResult, true))
+                        result.add(partResult.join(true))
                     }
                 } else {
-                    result.add(join(partResult, false))
+                    result.add(partResult.join(false))
                 }
             }
             return if (result.size > 1) {
@@ -153,18 +167,28 @@ class CssConverter {
             } else result.joinToString(", ")
         }
 
-        private fun processValue(input: String): String {
-            return maybeWrapIntoQuotes(trimQuotes(removeSuffix(input)).replace("'", "\\'"))
+        private fun determineType() {
+
         }
 
-        private fun join(input: ArrayList<String>, wrap: Boolean = false, multiline: Boolean = false): String {
-            return if (wrap) {
-                if (multiline)
-                    input.joinToString(",\n", "[\n", "\n]")
-                else
-                    input.joinToString(", ", "[", "]")
-            } else
-                input.joinToString(", ")
+        private fun processValue(input: String): String {
+            return maybeWrapIntoQuotes(
+                input.removeSuffix("px")
+                    .trimQuotes()
+                    .replace("'", "\\'")
+            )
+        }
+
+        private fun ArrayList<String>.join(wrap: Boolean = false, multiline: Boolean = false): String {
+            return when {
+                wrap -> {
+                    if (multiline)
+                        this.joinToString(",\n", "[\n", "\n]")
+                    else
+                        this.joinToString(", ", "[", "]")
+                }
+                else -> this.joinToString(", ")
+            }
         }
 
         private fun containsArrays(input: ArrayList<String>): Boolean {
--- src/main/kotlin/org/murtukov/css_to_jss/util/Extensions.kt
+++ src/main/kotlin/org/murtukov/css_to_jss/util/Extensions.kt
@@ -1,6 +1,10 @@
 package org.murtukov.css_to_jss.util
 
 /**
+ * TODO: Move extensions to CssConverter class
+ */
+
+/**
  * Removes redundant whitespaces from string and trims it.
  */
 fun String.normalizeWhitespaces(): String {
@@ -15,7 +19,7 @@ fun String.removeCommentBlocks(): String {
 
     while (true) {
         val start = result.indexOf("/*")
-        val end = result.indexOf("*/")
+        val end   = result.indexOf("*/")
 
         result = if (-1 != start && -1 != end) {
             result.substringBefore("/*") + result.substringAfter("*/")
@@ -23,4 +27,24 @@ fun String.removeCommentBlocks(): String {
     }
 
     return result;
+}
+
+/**
+ * Removes single or double quotes from the start and the end of a string.
+ */
+fun String.trimQuotes() = this.removeSurrounding("'").removeSurrounding("\"")
+
+/**
+ * Checks if given string contains only digits and max 1 point
+ */
+fun String.isNumeric() = this.toDoubleOrNull() != null
+
+/**
+ * Converts a kebab-cased string into a camelCased one.
+ */
+fun String.toCamelCase(): String {
+    return this
+        .split('-')
+        .joinToString("") { it.capitalize() }
+        .decapitalize()
 }
